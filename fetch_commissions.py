a997314abc586cf98e0dfe255c0f98b8bbb0b2b6
georgiygusev <georgiy.gus@yandex.ru>
2025-07-19 19:05:19 +0300

    raise for status

--- fetch_commissions.py
+++ fetch_commissions.py
@@ -1,15 +1,14 @@
 import aiohttp
 import json
 
+
 async def fetch_commissions(api_token: str) -> tuple[str, str]:
     url = "https://common-api.wildberries.ru/api/v1/tariffs/commission"
     headers = {"Authorization": api_token}
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
-            if response.status != 200:
-                text = await response.text()
-                raise Exception(f"WB API error: {response.status} — {text}")
+            response.raise_for_status()
             data = await response.json()
 
     return json.dumps(data, indent=2)
